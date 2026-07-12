d938ee4b9ab4e2507ddb3bd0ca8493dba941e62f
osama <you@example.com>
2026-07-12 20:44:44 +0300

    fixs

--- part2/hbnb/tests/test_users.py
+++ part2/hbnb/tests/test_users.py
@@ -5,7 +5,7 @@ from app.services import facade
 class TestUserEndpoints(unittest.TestCase):
 
     def setUp(self):
-        """Test setup: create app and test client, reset user repository."""
+        """Test setup method to initialize the test client and clear the user repository."""
         self.app = create_app()
         self.client = self.app.test_client()
         facade.user_repo._storage = {}
@@ -22,7 +22,7 @@ class TestUserEndpoints(unittest.TestCase):
         self.assertIn("id", response.json)
 
     def test_create_user_invalid_email(self):
-        """Test creating user with invalid (duplicate) email."""
+        """test creating a user with an invalid email format."""
         self.client.post('/api/v1/users/', json={
             "first_name": "Osama",
             "last_name": "Alhamdan",
