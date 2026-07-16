b30d522c90cfa47ec73aa93656f1d7200b25a3d2
ARAlsalhi <aalsalhi.eng@gmail.com>
2026-07-16 20:32:32 +0300

    add  API namespace  in init

--- part2/hbnb/app/__init__.py
+++ part2/hbnb/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask
 from flask_restx import Api
+from app.api.v1.users import api as users_ns
+from app.api.v1.amenities import api as amenities_ns
+from app.api.v1.places import api as places_ns
+from app.api.v1.reviews import api as reviews_ns
 
 def create_app():
     app = Flask(__name__)
